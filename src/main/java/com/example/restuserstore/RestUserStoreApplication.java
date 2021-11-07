53c52fcf664188e033257aa13ad3bc8c0e43bb10
Anisuzzaman <anisuzzamanbabla@gmail.com>
2021-11-07 11:53:24 +0600

    Initial Commit

---
+++ src/main/java/com/example/restuserstore/RestUserStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.restuserstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUserStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestUserStoreApplication.class, args);
+    }
+
+}
